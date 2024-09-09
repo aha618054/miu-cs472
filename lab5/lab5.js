ffc17b4739274c68e4809ed80f1f7e3ec5a6e1af
AnhTuanHa <tuanha17897@gmail.com>
2024-09-08 22:59:59 -0500

    updated lab5

---
+++ lab5/lab5.js
@@ -0,0 +1,59 @@
+//-----------------------1-----------------------
+class Meditation {
+    constructor(duration) {
+      this.duration = duration;
+    }
+  
+    start() {
+      console.log(`Start meditation`);
+      let countdown = this.duration;
+  
+      const intervalId = setInterval(() => {
+        if (countdown > 0) {
+          console.log(countdown);
+          countdown--;
+        } else {
+          console.log('Jay Guru Dev');
+          clearInterval(intervalId); // Stops the interval when the countdown is over
+        }
+      }, 1000); // 1000 milliseconds (1 second) interval
+    }
+  }
+  
+  // Usage
+  const morning_meditation = new Meditation(5);
+  morning_meditation.start();
+  
+  //-----------------------2-----------------------
+  const isPrime = (n) => {
+    return new Promise((resolve, reject) => {
+      if (n < 2) {
+        reject({ prime: false });
+        return;
+      }
+  
+      for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
+        if (n % i === 0) {
+          reject({ prime: false });
+          return;
+        }
+      }
+  
+      resolve({ prime: true });
+    });
+  };
+  const testPrime = async (n) => {
+    console.log('start');
+    try {
+      const result = await isPrime(n);
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+    console.log('end');
+  };
+  
+  // Testing with async/await
+  testPrime(7);  // Prime number
+  testPrime(4);  // Not a prime number
+  
